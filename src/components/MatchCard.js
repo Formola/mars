5a782e8f65e9a9d119a31e34509b9f901704406a
nandotiamat <tambu00matt@gmail.com>
2022-08-25 19:49:55 +0200

    fix matchcard mobile

--- src/components/MatchCard.js
+++ src/components/MatchCard.js
@@ -1,19 +1,20 @@
 import React from "react";
 import { Link } from "react-router-dom";
+import "../css/matchcard.css"
 
 export const MatchCard = ({ match, key }) => {
   return match ? (
     <Link key={key} to="/match" state={match}>
     <div
-      className="is-clickable content has-background-primary box is-flex is-justify-content-space-between has-text-white m-3"
+      className="is-clickable content matchcards has-background-primary box is-flex  is-justify-content-space-between has-text-white m-3 is-flex-direction-column-mobile"
     >
       <div className="is-flex is-flex-direction-column">
-        <span className="is-size-3">{match.winner.username}</span>
+        <span className="is-size-3 is-size-4-mobile">{match.winner.username}</span>
         <span className="is-size-6">
           {"As " + match.winner.points.corporation}
         </span>
       </div>
-      <p className="is-size-1">{match.winner.points.finalPoints}</p>
+      <span className="has-text-centered is-size-1">{match.winner.points.finalPoints}</span>
     </div>
     </Link>
     
